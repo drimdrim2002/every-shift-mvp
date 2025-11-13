58c2290a1a7a2a012ae576b6a65f95bcb66fbee6
drimdrim2002 <drimdrim2002@naver.com>
2025-11-14 00:28:37 +0900

    그리드 및 상태 관리 패키지 설치 (TanStack Table, Pinia, Vue Router)"

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
 import './style.css'
 import App from './App.vue'
 
@@ -6,5 +8,11 @@ import App from './App.vue'
 import naive from 'naive-ui'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+// Register plugins
+app.use(pinia)
+app.use(router)
 app.use(naive)
+
 app.mount('#app')
